2968f0fd170dffa54df21478bc004692a83b7ba5
digitalhigh <d8ahazard@gmail.com>
2017-03-21 10:27:46 -0500

    Change random key gen order for Docker
    See if this will get it running...

--- login.php
+++ login.php
@@ -216,12 +216,12 @@ function write_log2($text,$level=null) {
 	    if(!isset($length) || intval($length) <= 8 ){
 	      $length = 32;
 	    }
-	    if (function_exists('random_bytes')) {
-	        return bin2hex(random_bytes($length));
-	    }
 	    if (function_exists('mcrypt_create_iv')) {
 	        return bin2hex(mcrypt_create_iv($length, MCRYPT_DEV_URANDOM));
 	    } 
+		if (function_exists('random_bytes')) {
+	        return bin2hex(random_bytes($length));
+	    }
 	    if (function_exists('openssl_random_pseudo_bytes')) {
 	        return bin2hex(openssl_random_pseudo_bytes($length));
 	    }
