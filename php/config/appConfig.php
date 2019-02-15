01fce9237e727deb69fc81da56cb77687e2ee2a8
d8ahazard <donate.to.digitalhigh@gmail.com>
2019-02-15 08:51:31 -0600

    Moar cleanup, fixes.

--- php/config/appConfig.php
+++ php/config/appConfig.php
@@ -19,31 +19,31 @@ class appConfig
      */
 	public function __construct($configFile, $type) {
 		$configObject = false;
-       if (!file_exists($configFile) || !is_readable($configFile)) {
-           throw new ConfigException("Invalid config file specified.");
-       }
-       $configFile = realpath($configFile);
-			if ($type === 'db') {
-	$config = str_replace("'; <?php die('Access denied'); ?>", "", file_get_contents($configFile));
-	$config = json_decode($config, true);
-       if ($config) {
-           $user = $config['username'] ?? false;
-           $pass = $config['password'] ?? false;
-					$dbName = $config['database'] ?? false;
-           if (!$user || !$pass || !$dbName) {
-               $words = [];
-               if (!$user) array_push($words, "'username'");
-               if (!$pass) array_push($words,"'password'");
-               if (!$dbName) array_push($words,"'dbname'");
-               $strings = join(", ",$words);
-               throw new ConfigException("Missing config param(s) $strings");
-           } else {
-		$configObject = new DbConfig($config);
-           }
-				}
-       } else {
-           $configObject = new JsonConfig($configFile);
-       }
+		if (!file_exists($configFile) || !is_readable($configFile)) {
+		   throw new ConfigException("Invalid config file specified.");
+		}
+		$configFile = realpath($configFile);
+		if ($type === 'db') {
+			$config = str_replace("'; <?php die('Access denied'); ?>", "", file_get_contents($configFile));
+			$config = json_decode($config, true);
+			if ($config) {
+			   $user = $config['username'] ?? false;
+			   $pass = $config['password'] ?? false;
+						$dbName = $config['database'] ?? false;
+			   if (!$user || !$pass || !$dbName) {
+			       $words = [];
+			       if (!$user) array_push($words, "'username'");
+			       if (!$pass) array_push($words,"'password'");
+			       if (!$dbName) array_push($words,"'dbname'");
+			       $strings = join(", ",$words);
+			       throw new ConfigException("Missing config param(s) $strings");
+			   } else {
+					$configObject = new DbConfig($config);
+			   }
+			}
+		} else {
+		   $configObject = new JsonConfig($configFile);
+		}
        $this->ConfigObject = $configObject;
     }
 }
